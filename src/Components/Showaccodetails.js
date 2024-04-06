1a627af1371484f9b081f3eaef983cdb33529dc5
Hritik kumar Sinha <hs8985@srmist.edu.in>
2024-04-06 21:08:17 +0530

    Added Fonts and layout changed

--- src/Components/Showaccodetails.js
+++ src/Components/Showaccodetails.js
@@ -32,23 +32,26 @@ function AccommodationDetails() {
 
   return (
     <div>
-      <h2>Accommodation Details</h2>
+      <div style={{fontFamily:'serif'}}> Accommodation Details</div>
       {accommodation && (
-        <div>
-           {accommodation.image && <img src={require('../../uploads/' +(accommodation.image))} alt="Accommodation" />}
-          <p><strong>Name:</strong> {accommodation.propertyName}</p>
+         <div className='d-flex'>
+          
+         <div className="card mr-5"style={{borderRadius: '5px',width:'35rem',display:'flex',justifyContent:'center',margin:'auto',boxShadow: '0 0 40rem rgba(0.4, 0.6, 0.7, 0.5)'}}> {accommodation.image && <img src={require('../../uploads/' +(accommodation.image))}style={{border:'25px solid #021a40'}} alt="Accommodation" />}</div>
+         <div className='p-5 mt-5 mb-3'style={{maxWidth:'50%', borderRadius: '15px', boxShadow: '0 0 40px rgba(0.2, 0.3, 0.4, 1)'}}>
+          <p> <strong><div className='text'style={{fontFamily:'sans-serif',fontSize:'2rem'}}>{accommodation.propertyName}</div></strong></p>
           <p><strong>Address:</strong> {accommodation.address}, {accommodation.city}, {accommodation.state}, {accommodation.pincode}</p>
           <p><strong>Description:</strong> {accommodation.description}</p>
           <p><strong>Property Type:</strong> {accommodation.propertyType}</p>
           <p><strong>Room Type:</strong> {accommodation.roomType}</p>
-          <p><strong>Amenities:</strong> {Object.keys(accommodation.amenities).join(', ')}</p>
+          
           <p><strong>More Info:</strong> {accommodation.moreInfo}</p>
           <p><strong>Check-In Time:</strong> {accommodation.checkInTime}, <strong>Check-Out Time:</strong> {accommodation.checkOutTime}</p>
           <p><strong>Maximum Guests:</strong> {accommodation.maxGuests}</p>
           <p><strong>Price:</strong> Rs. {accommodation.price}</p>
         </div>
+        </div>
       )}
-      <Link to='/profile' className='btn btn-primary'>back</Link>
+      <Link to='/profile' className='btn btn-primary mb-4'>Go back</Link>
     </div>
   );
 }
